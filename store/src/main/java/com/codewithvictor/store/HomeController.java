ff105fab93e28a2e8807ce73406440576d806341
ub-victor <victoireushindi371@gmail.com>
2026-09-13 16:09:43 +0200

    Initial Commit

--- store/src/main/java/com/codewithvictor/store/HomeController.java
+++ store/src/main/java/com/codewithvictor/store/HomeController.java
@@ -1,5 +1,10 @@
 package com.codewithvictor.store;
+import org.springframework.web.bind.annotation.
 
 @Controller
 public class HomeController {
+    @RequestMapping("/")
+    public String Index() {
+        return "index.html";
+    }
 }
